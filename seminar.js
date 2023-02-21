e163a77152f861ebde6ca1a26ef5dc81129db063
Abinesh Jain <amsiabinesh@gmail.com>
2023-02-21 23:00:53 +0530

    Create seminar.js

---
+++ seminar.js
@@ -0,0 +1,37 @@
+// Abinesh Jain A J
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// Fuction to add the values in an array
+function sum(total, currentValue) {
+    return total + currentValue;
+}
+
+// Function to check if a number is greater than 0
+function greaterThanZero(value) {
+    return value > 0;
+}
+
+// reduce method
+
+// let result = arr.reduce(sum, 0);
+
+// reduceRight method
+
+// let result = arr.reduceRight(sum);
+
+// shift method
+
+// let result=arr.shift();
+
+// slice method
+
+// let result=arr.slice(2,4);
+
+// some method
+
+// let result = arr.some(greaterThanZero);
+
+
+console.log(result);
+console.log(arr);
